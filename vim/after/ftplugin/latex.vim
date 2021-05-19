6af850dc97485e6c6a611dd849c473ff1e029388
Carlos <carlos.olivares.331@outlook.com>
2021-05-19 15:47:56 -0400

    Symlinked vim files
    I took a lot longer than I thought, but I have the vim files and they
    are setup so that any edits to one edits the other (i think).
    At the very least, if I change the .vim directory, it will show up in
    this repository.

--- vim/after/ftplugin/latex.vim
+++
@@ -1,2 +0,0 @@
-
-nnoremap ,art :-1read ~/.vim/vim_snippets/latex_snippets/article_skeleton.txt<CR>gg/title{<CR>f{ci{
--- vim/vim_snippets/python_snippets/fn-with-docstring.py
+++
@@ -1,8 +0,0 @@
-def :
-    """ One line description of what function does
-    Arguments:
-        param1: parameter type
-        param2: paremeter type
-    Returns:
-        Description of return. Does not need to include type
-    """
